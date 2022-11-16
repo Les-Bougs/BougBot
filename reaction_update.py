6c3af45ec3e9a8f066e2b3018ed1f7828a289f8e
aightech <long.devillard@gmail.com>
2022-11-16 15:52:35 +0000

    fixed gitignore

--- reaction_update.py
+++ reaction_update.py
@@ -34,20 +34,31 @@ def reaction_update(bot, reaction, user):
             print(f"{user} already tipped this msg")
         else:
             boug_msg.tipers.append(user)
-            #add a timestamp to the dict_tip_ts of the boug
-            if(bot.dict_boug[int(message.author.id)].dict_tip_ts.get(user.id) is None):
-                bot.dict_boug[int(message.author.id)].dict_tip_ts[user.id] = message.created_at
+            # add a timestamp to the dict_tip_ts of the boug
+            print(
+                f"{user} tipped {bot.dict_boug[int(message.author.id)].dict_tip_ts.get(user.id)}")
+            if (bot.dict_boug[int(message.author.id)].dict_tip_ts.get(user.id) is None):
+                bot.dict_boug[int(message.author.id)
+                              ].dict_tip_ts[user.id] = message.created_at
+                print(
+                    f"IT'S TIP TIME !! {message.author} gagne +1BGC grâce au tip de {user}")
+                give_money(bot, source=user, target=message.author, amount=1)
             else:
                 max_period = 10
-                if((message.created_at - bot.dict_boug[int(message.author.id)].dict_tip_ts[user.id]).seconds < max_period):
-                    print(f"{user} already tipped this boug in the last {max_period} seconds")
+                print(
+                    f"last tip: {bot.dict_boug[int(message.author.id)].dict_tip_ts[user.id]}")
+                if ((message.created_at - bot.dict_boug[int(message.author.id)].dict_tip_ts[user.id]).seconds < max_period):
+                    print(
+                        f"{user} already tipped this boug in the last {max_period} seconds")
                     return
                 else:
-                    bot.dict_boug[int(message.author.id)].dict_tip_ts[user.id] = message.created_at
+                    bot.dict_boug[int(message.author.id)
+                                  ].dict_tip_ts[user.id] = message.created_at
                     print(
                         f"IT'S TIP TIME !! {message.author} gagne +1BGC grâce au tip de {user}"
                     )
-                    give_money(bot, source=user, target=message.author, amount=1)
+                    give_money(bot, source=user,
+                               target=message.author, amount=1)
     else:
         print(f"{user} paye {message.author} en visibilité")
 
